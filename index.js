d6deb5503c7ae0953bcb922ba6168b328e9cf665
Jbunz <jacob652@aol.com>
2024-01-14 12:09:06 -0800

    add function

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-function newImage(imageSrc, positionLeft, positionBottom) {
-    let greenCharacter = document.createElement('img')
-    greenCharacter.src = 'assets/green-character.gif'
-    greenCharacter.style.position = 'fixed'
-    greenCharacter.style.left = '100px'
-    greenCharacter.style.bottom = '100px'
-    document.body.append(greenCharacter)
-    
-    let pineTree = document.createElement('img')
-    pineTree.src = 'assets/pine-tree.png'
-    pineTree.style.position = 'fixed'
-    pineTree.style.left = '450px'
-    pineTree.style.bottom = '200px'
-    document.body.append(pineTree) 
+function createImage(src, positionLeft, positionBottom) {
+  let image = document.createElement("img");
+  image.src = src;
+  image.style.position = "fixed";
+  image.style.left = positionLeft;
+  image.style.bottom = positionBottom;
+  document.body.append(image);
+  return image;
 }
 
-newImage('assets/character.png', '200px', '100px');
+function newImage(imageSrc, positionLeft, positionBottom) {
+  createImage(imageSrc, positionLeft, positionBottom);
+}
 
-newImage('assets/pine-tree.png', '450px', '200px');
+newImage("assets/green-character.gif", "200px", "100px");
+newImage("assets/pine-tree.png", "450px", "200px");
+newImage("assets/tree.png", "200px", "300px");
+newImage("assets/pillar.png", "350px", "100px");
+newImage("assets/crate.png", "150px", "200px");
+newImage("assets/well.png", "500px", "425px");
+newImage("assets/sword.png", "500px", "405px");
+
+function newItem(itemSrc, positionLeft, positionBottom, onClick) {
+  let item = createImage(itemSrc, positionLeft, positionBottom);
+  item.addEventListener("click", function () {
+    if (onClick) {
+      onClick(item);
+    } else {
+      item.remove();
+    }
+  });
+}
+
+newItem("assets/sword.png", "500px", "405px", function (sword) {
+  sword.remove();
+});
